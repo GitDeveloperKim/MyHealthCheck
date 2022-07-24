0ade5aa079878e507533bbc822d424c5e79ae68a
ys88sem.kim <ys88sem.kim@samsung.com>
2022-07-24 22:08:06 +0900

    add recyclerview event listener (Toast message)

--- app/src/main/java/com/example/myapplication01/ui/adapter/HealthAdapter.kt
+++ app/src/main/java/com/example/myapplication01/ui/adapter/HealthAdapter.kt
@@ -3,6 +3,7 @@ package com.example.myapplication01.ui.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication01.R
 import com.example.myapplication01.vo.HealthItem
@@ -24,15 +25,19 @@ class HealthAdapter (val healthItems: MutableList<HealthItem>):
 
     // 뷰 홀더의 뷰에 데이터를 출력하려고 자동으로 호출
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(healthItems[position])
+        val listener = View.OnClickListener {
+            Toast.makeText(it.context, healthItems[position].healthName, Toast.LENGTH_SHORT).show()
+        }
+        holder.bind(listener, healthItems[position])
     }
 
     // 항목 개수를 판단하려고 자동으로 호출
     class ViewHolder(var view: View): RecyclerView.ViewHolder(view) {
-        fun bind(item:HealthItem) {
+        fun bind(listener: View.OnClickListener, item:HealthItem) {
             view.health_image.setImageResource(item.healthImage)
             view.health_name.text = item.healthName
             view.health_description.text = item.healthDescription
+            view.setOnClickListener(listener)
         }
     }
 }
